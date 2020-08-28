eacf9f761c1f7e0d37e40bb4af42ad9b10b13f6a
Hristiyan Dodov <h.dodov@gmail.com>
2020-08-28 11:21:56 +0300

    fix(exporter): skip empty structures

--- routes/export.php
+++ routes/export.php
@@ -3,29 +3,44 @@
 namespace Oblik\Memsource;
 
 use Exception;
+use Kirby\Cms\Structure;
 use Oblik\Walker\Util\Diff;
 use Oblik\Walker\Walker\Exporter;
 use Oblik\Walker\Walker\Importer;
 
 class MemsourceExporter extends Exporter
 {
-	protected function fieldHandler($field, $settings, $input)
-	{
-		$data = parent::fieldHandler($field, $settings, $input);
-
-		if (!empty($data)) {
-			return $data;
-		} else {
-			return null;
-		}
-	}
-
 	protected function fieldPredicate($field, $settings, $input)
 	{
 		$pass = parent::fieldPredicate($field, $settings, $input);
 		$translate = $settings['translate'] ?? true;
 
 		return ($pass && $translate);
+	}
+
+	protected function fieldHandler($field, $settings, $input)
+	{
+		$data = parent::fieldHandler($field, $settings, $input);
+
+		if (empty($data)) {
+			$data = null;
+		}
+
+		return $data;
+	}
+
+	protected function structureHandler(Structure $structure, array $blueprint, $input, $sync)
+	{
+		$data = parent::structureHandler($structure, $blueprint, $input, $sync);
+
+		// The data is not filtered with `array_filter()` by default because if
+		// empty entries are removed, all non-empty entries will be located at a
+		// different index. However, if *all* entries are empty, return null.
+		if (is_array($data) && count(array_filter($data)) === 0) {
+			$data = null;
+		}
+
+		return $data;
 	}
 }
 
